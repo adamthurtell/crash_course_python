6829b016257168b9121f5c6a94dfdeccf570b4f9
adamthurtell <adamthurtell@gmail.com>
2017-03-26 15:20:45 -0700

    work on 3/26

---
+++ favorite_numbers.py
@@ -0,0 +1,11 @@
+favorite_numbers0 = {
+    'freddie': 10,
+    'payal': 4,
+    'adam': 19,
+    'abe': 27,
+    'jack': 3,
+}
+
+for k, v in favorite_numbers0.items():
+    print("\nName: " + k.title())
+    print("Favorite Number: " + str(v))
---
+++ python_glossary.py
@@ -0,0 +1,20 @@
+python_glossary0 = {
+    'for loop': 'def for loop',
+    'dictionary': 'def dictionary',
+    'if statements': 'def if statements',
+    'elif statements': 'def elif statements',
+    'else statements': 'def else statements',
+    'list': 'def list',
+    'while loops': 'while loops not yet defined',
+    'functions': 'functions not yet defined',
+    'classes': 'classes not yet defined',
+}
+
+print("Glossary of Python Terms:")
+
+for k, v in sorted(python_glossary0.items()):
+    print("\n" + k.title() + ":")
+    print(v)
+
+python_glossary0['git'] = 'def of git'
+print(python_glossary0)
