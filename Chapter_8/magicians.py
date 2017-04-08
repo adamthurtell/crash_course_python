feeab3163abce06a222484ceac44bd25abffac07
adamthurtell <adamthurtell@gmail.com>
2017-04-08 15:49:25 -0700

    more work 4/8

---
+++ Chapter_8/magicians.py
@@ -0,0 +1,7 @@
+def show_magicians(magicians):
+    print("\nThe names of the magicians are:")
+    for magician in magicians:
+        print(magician)
+
+magicians = ['David Copperfield', 'David Blaine', 'Houdini']
+show_magicians(magicians)
---
+++ Chapter_8/magicians_2.py
@@ -0,0 +1,20 @@
+def show_magicians(magicians):
+    print("\nThe names of the magicians are:")
+    for magician in magicians:
+        print(magician)
+
+def make_great(magicians, great_magicians):
+    """Add 'the Great' to Magicians names"""
+    while magicians:
+        great_magician = magicians.pop()
+
+        print("\n" + great_magician + " is becoming great...")
+        great_magician = great_magician + " the Great"
+        great_magicians.append(great_magician)
+
+magicians = ['David Copperfield', 'David Blaine', 'Houdini']
+great_magicians = []
+show_magicians(magicians)
+make_great(magicians[:], great_magicians)
+show_magicians(great_magicians)
+show_magicians(magicians)
